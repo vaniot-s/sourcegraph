98b9e81588add1dbe58d8919d70961a358190acb
Nick Snyder <nick@sourcegraph.com>
2018-11-21 12:06:19 -0800

    wip

---
+++ web/src/site-admin/SiteAdminExternalServicesPage.tsx
@@ -0,0 +1,105 @@
+import { map } from 'lodash'
+import * as React from 'react'
+import { RouteComponentProps } from 'react-router'
+import { Observable, Subject } from 'rxjs'
+import { gql } from '../../../shared/src/graphql/graphql'
+import * as GQL from '../../../shared/src/graphql/schema'
+import { createAggregateError } from '../../../shared/src/util/errors'
+import { queryGraphQL } from '../backend/graphql'
+import { FilteredConnection, FilteredConnectionQueryArgs } from '../components/FilteredConnection'
+import { PageTitle } from '../components/PageTitle'
+import { eventLogger } from '../tracking/eventLogger'
+
+interface ExternalServiceNodeProps {
+    node: GQL.IExternalService
+    onDidUpdate?: () => void
+}
+
+interface ExternalServiceNodeState {
+    // loading: boolean
+    // errorDescription?: string
+}
+
+class ExternalServiceNode extends React.PureComponent<ExternalServiceNodeProps, ExternalServiceNodeState> {
+    public state: ExternalServiceNodeState = {
+        // loading: false,
+    }
+
+    public render(): JSX.Element | null {
+        return <li className="repository-node list-group-item py-2">{this.props.node.displayName}</li>
+    }
+}
+
+interface Props extends RouteComponentProps<{}> {}
+interface State {}
+
+class FilteredExternalServiceConnection extends FilteredConnection<GQL.IExternalServiceConnection> {}
+
+/**
+ * A page displaying the external services on this site.
+ */
+export class SiteAdminExternalServicesPage extends React.PureComponent<Props, State> {
+    public state: State = {}
+
+    // private repositoryUpdates = new Subject<void>()
+
+    public componentDidMount(): void {
+        eventLogger.logViewEvent('SiteAdminExternalServices')
+    }
+
+    // public componentWillUnmount(): void {}
+
+    public render(): JSX.Element | null {
+        const nodeProps: Pick<ExternalServiceNodeProps, 'onDidUpdate'> = {
+            // onDidUpdate: this.onDidUpdateRepository,
+        }
+
+        return (
+            <div className="site-admin-external-services-page">
+                <PageTitle title="External Services - Admin" />
+                <h2>External Services</h2>
+                <p>Manage connections to external services.</p>
+                <FilteredExternalServiceConnection
+                    className="list-group list-group-flush mt-3"
+                    noun="external service"
+                    pluralNoun="external services"
+                    queryConnection={this.queryExternalServices}
+                    nodeComponent={ExternalServiceNode}
+                    nodeComponentProps={nodeProps}
+                    // updates={this.repositoryUpdates}
+                    history={this.props.history}
+                    location={this.props.location}
+                />
+            </div>
+        )
+    }
+
+    private queryExternalServices = (args: FilteredConnectionQueryArgs): Observable<GQL.IExternalServiceConnection> =>
+        queryGraphQL(
+            gql`
+                query ExternalServices($first: Int) {
+                    externalServices(first: $first) {
+                        nodes {
+                            displayName
+                        }
+                        totalCount
+                        pageInfo {
+                            hasNextPage
+                        }
+                    }
+                }
+            `,
+            {
+                first: args.first,
+            }
+        ).pipe(
+            map(({ data, errors }) => {
+                if (!data || !data.externalServices || errors) {
+                    throw createAggregateError(errors)
+                }
+                return data.externalServices
+            })
+        )
+
+    // private onDidUpdateRepository = () => this.repositoryUpdates.next()
+}
