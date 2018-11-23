b76e5839471a897f07ab9a69893051a50fdb0023
Beyang Liu <beyang@sourcegraph.com>
2018-11-22 17:29:24 -0800

    .

--- pkg/extsvc/github/orgs_test.go
+++ pkg/extsvc/github/orgs_test.go
@@ -1,7 +1,46 @@
 package github
 
-import "testing"
+import (
+	"context"
+	"reflect"
+	"testing"
+)
 
 func TestListOrgs(t *testing.T) {
-	// TODO
+	accessToken := "this-is-the-access-token"
+	mock := mockHTTPResponseBody{
+		responseBody: `
+[
+  {
+    "login": "github",
+    "id": 1,
+    "node_id": "MDEyOk9yZ2FuaXphdGlvbjE=",
+    "url": "https://api.github.com/orgs/github",
+    "repos_url": "https://api.github.com/orgs/github/repos",
+    "events_url": "https://api.github.com/orgs/github/events",
+    "hooks_url": "https://api.github.com/orgs/github/hooks",
+    "issues_url": "https://api.github.com/orgs/github/issues",
+    "members_url": "https://api.github.com/orgs/github/members{/member}",
+    "public_members_url": "https://api.github.com/orgs/github/public_members{/member}",
+    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+    "description": "A great organization"
+  }
+]
+`}
+
+	c := newTestClient(t)
+	c.httpClient.Transport = &mock
+
+	want := Organization{
+		// TODO
+	}
+
+	orgs, err := c.ListOrgs(context.Background(), accessToken)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(want, orgs) {
+		// TODO: spew
+		t.Errorf("want %+v, got %+v", want, orgs)
+	}
 }
